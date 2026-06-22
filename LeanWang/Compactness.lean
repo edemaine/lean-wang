9742dfd476e299215d42a4fa9eacf03e00414f5e
Erik Demaine <edemaine@mit.edu>
2026-06-21 21:10:09 -0400

    Apache license

--- LeanWang/Compactness.lean
+++ LeanWang/Compactness.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 lean-wang contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Erik Demaine, OpenAI
+Authors: Erik Demaine, Stefan Langerman, GPT 5.5
 -/
 import LeanWang.Basic
 import Mathlib.Data.Int.Interval
