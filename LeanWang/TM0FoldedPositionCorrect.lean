6946e6b784c3a36d45dc17379ce7b0c62e518656
Erik Demaine <edemaine@mit.edu>
2026-07-02 18:47:26 -0400

    Import compiler correctness directly

--- LeanWang/TM0FoldedPositionCorrect.lean
+++ LeanWang/TM0FoldedPositionCorrect.lean
@@ -3,7 +3,7 @@ Copyright (c) 2026 lean-wang contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Erik Demaine, Stefan Langerman, GPT 5.5
 -/
-import LeanWang.TM0FoldedCompiler
+import LeanWang.TM0FoldedCompiler.Correctness
 import LeanWang.TM0FoldedGeneratedInit
 
 /-!
